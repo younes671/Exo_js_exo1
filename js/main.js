824c9523e68051d606d6ac08f885f94405091e8f
Younesse AL AMRI <younes671@live.fr>
2024-02-05 09:06:31 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.querySelector(".carre").addEventListener("click", function(){
+    alert("Class : carre \n-Background color : aqua \n-Color : blueViolet \n-Height : 200 \n-Width : 200 \n-Display : flex \n -Display : 'Times New Roman' (40px)")
+})
